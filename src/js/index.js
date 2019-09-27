3192db774a6b7559590cbad31987a94a1eb6ba36
Ahmad Kamal <Ahmad106107@gmail.com>
2019-09-27 16:34:13 +0200

    babel and webpack

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import str from './views/searchView';
+import { add, multiply, ID } from './modules/Search'
+console.log(`added numbers ${add(ID, 5)} multiplied numbers are ${multiply(5, ID)} and the string from SearchView is ${str}`)
