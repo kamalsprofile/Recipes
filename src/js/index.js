c0ac47becd6110d963131f2bf205a9ff9b09f5ca
Ahmad Kamal <Ahmad106107@gmail.com>
2019-09-27 17:16:24 +0200

    API call

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
-import str from './views/searchView';
-import { add, multiply, ID } from './modules/Search'
-console.log(`added numbers ${add(ID, 5)} multiplied numbers are ${multiply(5, ID)} and the string from SearchView is ${str}`)
+import axios from 'axios';
+//https://www.food2fork.com/api/search
+//07fe92a4e51f2f32c89aff7ea7d8eecd
+
+async function recipes(searchQuery) {
+    const key = 'b3ef3343426d3e9830968f03e3e3280d'
+
+    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${searchQuery}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+}
+
+recipes('pasta');
